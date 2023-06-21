b8e3090dfd2054dc70c97e2a009601e222f1a54b
mintaek22 <biba99@naver.com>
2023-06-21 18:38:32 +0900

    good

--- app/src/main/java/com/example/bigtemi/MainActivity.java
+++ app/src/main/java/com/example/bigtemi/MainActivity.java
@@ -24,12 +24,18 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
     DatabaseReference databaseReference = firebaseDatabase.getReference();
     private Robot robot;
     private ImageView imageView;
-    public MainActivity() {this.robot = Robot.getInstance();}
-    public Robot getRobot() {return robot;}
+
+    public MainActivity() {
+        this.robot = Robot.getInstance();
+    }
+
+    public Robot getRobot() {
+        return robot;
+    }
 
     MediaPlayer mediaPlayer;
-    int level,game,one_win,one_lose,two_win,two_lose;
-    int count = 3;
+    int level, game, one_win, one_lose, two_win, two_lose, sensor;
+    int count = 300;
     private boolean player_one_win, player_one_lose, player_two_win, player_two_lose, gamestart;
 
 
@@ -48,74 +54,91 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
         player_two_win = false;
         player_two_lose = false;
 
-        //temi앱에서 시작버튼을 눌렀을 때(game level에 따라 음성파일이 달라짐)
         databaseReference.child("game").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Object rdata = snapshot.getValue();
                 game = Integer.parseInt(rdata.toString());
-                //난이도 : 쉬움
-                if(game == 1) {
+                if(game==1 || game==2 || game==3){
+                    databaseReference.child("sensor").setValue(0);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //temi앱에서 시작버튼을 눌렀을 때(game level에 따라 음성파일이 달라짐)
+        databaseReference.child("sensor").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object rdata = snapshot.getValue();
+                sensor = Integer.parseInt(rdata.toString());
+                System.out.println("game"+game+"sensor"+sensor);
+                if (game == 1 && sensor == 0) {
                     level = 1;
                     playTemiSound();
                     gamestart = false;
                     rest(6000);
+                    databaseReference.child("sensor").setValue(1);
                 }
                 //난이도 : 중간
-                else if (game == 2) {
+                else if (game == 2 && sensor == 0) {
                     level = 2;
                     playTemiSound();
                     gamestart = false;
                     rest(4000);
+                    databaseReference.child("sensor").setValue(1);
                 }
                 //난이도 : 어려움
-                else if (game == 3) {
+                else if (game == 3 && sensor == 0) {
                     level = 3;
                     playTemiSound();
                     gamestart = false;
                     rest(2000);
+                    databaseReference.child("sensor").setValue(1);
                 }
                 //게임 종료 버튼 클릭시를 위한 변수값 초기화
-                else if (game == 0){
+                else if (game == 0) {
                     gamestart = true;
                     player_one_win = false;
                     player_one_lose = false;
                     player_two_win = false;
                     player_two_lose = false;
-                    count = 3;
+                    count = 300;
                 }
-                //temi에게 오는 신호인 game 변수값 초기화
-                game = -1;
                 //조교temi나 아두이노한테 센서 on으로 변경 신호 보냄
-                if(count >0){
-                    databaseReference.child("해당코드작성").setValue(1);
-                }
-                else{
+                if (count > 0) {
+                    //databaseReference.child("해당코드작성").setValue(1);
+                } else {
                     mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.gameover);
                     mediaPlayer.start();
                     //조교temi에게 게임종료 신호 전송
                     databaseReference.child("time_over").setValue(1);
                     //아두이노에게도 필요하면 전송
                 }
-
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
+
         //조교 temi로 부터 신호가 와서 1번 참가자 승리 음성 출력하고 게임 종료
         databaseReference.child("win/one").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Object rdata = snapshot.getValue();
                 one_win = Integer.parseInt(rdata.toString());
-                if(one_win == 1){
+                if (one_win == 1) {
                     gamestart = false;
                     player_one_win = true;
                     GameoverSound();
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -123,61 +146,69 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
         });
 
         //조교 temi로 부터 신호가 와서 1번 참가자 탈락 음성 출력 후 게임 재개
-        databaseReference.child("member/one").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Object rdata = snapshot.getValue();
-                one_lose = Integer.parseInt(rdata.toString());
-                if (one_lose == 0) {
-                    gamestart = false;
-                    player_one_lose = true;
-                    GameoverSound();
-                }
-            }
+        databaseReference.child("member/one").
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Object rdata = snapshot.getValue();
+                        one_lose = Integer.parseInt(rdata.toString());
+                        if (one_lose == 0) {
+                            gamestart = false;
+                            player_one_lose = true;
+                            GameoverSound();
+                        }
+                    }
 
-            }
-        });
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
         //조교 temi로 부터 신호가 와서 2번 참가자 승리 음성 출력하고 게임 종료
-        databaseReference.child("win/two").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Object rdata = snapshot.getValue();
-                two_win = Integer.parseInt(rdata.toString());
-                if(two_win == 1){
-                    gamestart = false;
-                    player_two_win = true;
-                    GameoverSound();
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        databaseReference.child("win/two").
 
-            }
-        });
+                addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Object rdata = snapshot.getValue();
+                        two_win = Integer.parseInt(rdata.toString());
+                        if (two_win == 1) {
+                            gamestart = false;
+                            player_two_win = true;
+                            GameoverSound();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
         //조교 temi로 부터 신호가 와서 1번 참가자 탈락 음성 출력 후 게임 재개
-        databaseReference.child("member/two").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Object rdata = snapshot.getValue();
-                two_lose = Integer.parseInt(rdata.toString());
-                if(two_lose == 0){
-                    gamestart = false;
-                    player_two_lose = true;
-                    GameoverSound();
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        databaseReference.child("member/two").
 
-            }
-        });
+                addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Object rdata = snapshot.getValue();
+                        two_lose = Integer.parseInt(rdata.toString());
+                        if (two_lose == 0) {
+                            gamestart = false;
+                            player_two_lose = true;
+                            GameoverSound();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
-    private void GameoverSound(){
+    private void GameoverSound() {
         if (player_one_lose) {
             mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.one_lose);
             mediaPlayer.start();
@@ -200,6 +231,7 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
             player_two_win = false;
         }
     }
+
     private void playTemiSound() {
         //게임 시작 버튼 클릭 시 "게임을 시작합니다" 음성 출력
         if (gamestart) {
@@ -222,21 +254,22 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
         count--;
     }
 
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         robot.addOnRobotReadyListener(this);
     }
 
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         robot.addOnRobotReadyListener(this);
     }
 
-    private void rest(int time){
+    private void rest(int time) {
         CountDownTimer timer = new CountDownTimer(time, 1000) {
             public void onTick(long millisUntilFinished) {
                 // 아무것도 하지 않음
             }
+
             public void onFinish() {
             }
         }.start();
@@ -249,12 +282,12 @@ public class MainActivity extends AppCompatActivity implements OnRobotReadyListe
         timer.cancel(); // 타이머 취소
     }
 
-    public void onRobotReady(boolean isReady){
-        if(isReady){
-            try{
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            try {
                 final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
                 robot.onStart(activityInfo);
-            } catch(PackageManager.NameNotFoundException e){
+            } catch (PackageManager.NameNotFoundException e) {
                 throw new RuntimeException(e);
             }
         }
